d313618e26bfdf22a9d70352e511ee7ac9198d16
Ponkin Dmitrij <ponkin.dmitrij@gmail.com>
2019-04-09 15:53:14 +0300

    Changes the size of the indentation

--- problems/001-sum.js
+++ problems/001-sum.js
@@ -13,17 +13,17 @@
  * @returns {number}
  */
 function sum(n) {
-    if (n < 0 || n === 0 || n === 1) {
-        return 1;
-    }
+  if (n < 0 || n === 0 || n === 1) {
+    return 1;
+  }
 
-    let sum = 0;
+  let sum = 0;
 
-    for (let i = 1; i <= n; i++) {
-        sum += i;
-    }
+  for (let i = 1; i <= n; i++) {
+    sum += i;
+  }
 
-    return sum;
+  return sum;
 }
 
 module.exports = sum;
